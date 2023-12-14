6d419d9ef1fd6d2ac70681977461e7f1c59856d1
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2023-12-14 15:58:33 -0300

    Imagen en la vista de eliminar blogpost (parte admin)

--- concepto-app/resources/views/admin/blog/delete.blade.php
+++ concepto-app/resources/views/admin/blog/delete.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.admin')
 
-@section('title', 'Concepto — Eliminar entrada del blog: '. $post->title)
+@section('title', 'Concepto — Eliminar entrada del blog: ' . $post->title)
 
 
 @section('content')
 
-<section class="container mt-100 mb-100">
+    <section class="container mt-100 mb-100">
 
-    <h1 class="h1 text-center mb-32">Confirmación para eliminar la entrada: "{{$post->title}}"</h1>
-    <p class="paragraph text-center bold m-0-auto mb-8">Resumen:</p>
-    <p class="paragraph mb-8 text-center max-600 m-0-auto mb-32">{{ $post->summary }}</p>
-    <p class="text-center paragraph-color-text bold mb-100">Por {{ $post->author->name }}</p>
+        <h1 class="h1 text-center mb-32">Confirmación para eliminar la entrada: "{{ $post->title }}"</h1>
 
-    <h2 class="h3 text-center">¿De verdad quieres eliminar esta entrada del blog?</h2>
-    <form action="{{ route('admin.blog.delete.process', ['id' => $post->id]) }}" method="post" id="form-checkout">
-        @csrf
-        <input type="submit" value="Eliminar entrada" class="main-cta mt-32 max-600 m-0-auto">
-    </form>
+        <div class="blog-img-container mb-64">
+            @if (Str::of($post->cover)->startsWith('https'))
+                <img src="{{ $post->cover }}" alt="{{ $post->title }}" class="blog-img">
+            @else
+                <img src="{{ asset('storage/' . $post->cover) }}" alt="{{ $post->title }}" class="blog-img">
+            @endif
+        </div>
 
-</section>
+        <p class="paragraph text-center bold m-0-auto mb-8">Resumen:</p>
+        <p class="paragraph mb-8 text-center max-600 m-0-auto mb-32">{{ $post->summary }}</p>
+        <p class="text-center paragraph-color-text bold mb-100">Por {{ $post->author->name }}</p>
+
+        <h2 class="h3 text-center">¿De verdad quieres eliminar esta entrada del blog?</h2>
+        <form action="{{ route('admin.blog.delete.process', ['id' => $post->id]) }}" method="post" id="form-checkout">
+            @csrf
+            <input type="submit" value="Eliminar entrada" class="main-cta mt-32 max-600 m-0-auto">
+        </form>
+
+    </section>
 
 @endsection
