0c77ea9b48b4468295b88821005947039bb1c0be
Brisa-Marca <brisa.marca@davinci.edu.ar>
2023-12-19 13:24:29 -0300

    modelos del checkout

---
+++ concepto-app/app/Models/Purchase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Purchase extends Model
+{
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function transaction(): BelongsTo
+    {
+        return $this->belongsTo(Transaction::class, 'transactions_id', 'id');
+    }
+}
