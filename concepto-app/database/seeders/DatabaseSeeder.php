d3952a9466839fe1b40b6c5f09766ffdce5610f2
Brisa-Marca <brisa.marca@davinci.edu.ar>
2023-12-19 16:24:04 -0300

    creación de la tabla de profesores y sus relación con la tabla courses

--- concepto-app/database/seeders/DatabaseSeeder.php
+++ concepto-app/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(TeacherSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(UserSeeder::class);
         $this->call(CourseSeeder::class);
