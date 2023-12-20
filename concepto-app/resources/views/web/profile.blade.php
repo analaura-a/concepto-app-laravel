e9024fb10d687c16d205b9a6682ee4c7b6771828
Ana <107949680+analaura-a@users.noreply.github.com>
2023-12-20 12:22:38 -0300

    Estilos del perfil (parte web)

--- concepto-app/resources/views/web/profile.blade.php
+++ concepto-app/resources/views/web/profile.blade.php
@@ -3,32 +3,27 @@
 @section('title', 'Concepto — Mi Perfil')
 
 @section('content')
-<section class="container mt-100 mb-100">
 
-    <h1 class="h1 h1-bigger text-center mb-56">Mi perfil</h1>
-    <p class="h3 mb-4 profile">Hola, <?= auth()->user()->email ?></p>
+    <section class="container mt-100 mb-100">
 
-    <ul class="content-profile-btn">
-    <li class="li-btn-profile">
-            <a href="{{url('perfil/'. auth()->user()->id. '/editar')}}" class="btn-profile">Modificar mis datos</a>
-        </li>
-        <li class="li-btn-profile">
-            <a href="{{ route('web.my_courses') }}" class="btn-profile">Mis cursos</a>
-        </li>
-        <li class="li-btn-profile">
-            <form action="{{ route('auth.web.logout.process') }}" method="post">
-            @csrf
-            <button type="submit" class="btn-logout">Cerrar sesión</button>
-            </form>
-        </li>
+        <h1 class="h1 h1-bigger text-center mb-56">Mi perfil</h1>
+        <p class="h3 profile m-0-auto">Hola, <?= auth()->user()->email ?></p>
 
+        <ul class="content-profile-btn">
+            <li class="li-btn-profile">
+                <a href="{{ url('perfil/' . auth()->user()->id . '/editar') }}" class="btn-profile">Modificar mis datos</a>
+            </li>
+            <li class="li-btn-profile">
+                <a href="{{ route('web.my_courses') }}" class="btn-profile">Mis cursos</a>
+            </li>
+            <li class="li-btn-profile">
+                <form action="{{ route('auth.web.logout.process') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn-logout">Cerrar sesión</button>
+                </form>
+            </li>
+        </ul>
 
-    </ul>
-
-
-
-
-</section>
-
+    </section>
 
 @endsection
