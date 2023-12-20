7d66b6f4e20161f97ece7f2920aaee99a213d6b5
Ana <107949680+analaura-a@users.noreply.github.com>
2023-12-20 13:39:13 -0300

    Estadística cursos más vendidos (parte admin)

--- concepto-app/app/Http/Controllers/AdminController.php
+++ concepto-app/app/Http/Controllers/AdminController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Course;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin/home');
+        $bestSellingCourses = Course::select('courses.course_id', 'courses.name', 'courses.cover', 'courses.short_description', 'courses.description', 'courses.price', DB::raw('COUNT(purchase_details.id) as total_sales'))
+            ->leftJoin('purchase_details', 'courses.course_id', '=', 'purchase_details.course_id')
+            ->groupBy('courses.course_id', 'courses.name', 'courses.cover', 'courses.short_description', 'courses.description', 'courses.price')
+            ->orderBy('total_sales', 'desc') // Ordena por total de ventas en orden ascendente
+            ->get();
+
+        return view('admin/home', [
+            'courses' => $bestSellingCourses,
+        ]);
     }
 }
--- concepto-app/resources/views/admin/home.blade.php
+++ concepto-app/resources/views/admin/home.blade.php
@@ -12,16 +12,46 @@
                 d="M29.775 14.521a5.556 5.556 0 0 0-4.727-.094l-9.09 4.059c-.609.271-1 .872-1 1.534v1.07c0 .639.363 1.222.938 1.506l7.348 3.64a1.704 1.704 0 0 0 1.512 0l7.412-3.671C33.47 21.92 35 22.86 35 24.305v1.372c0 .737-.42 1.411-1.084 1.74l-9.042 4.478a1.97 1.97 0 0 1-1.748 0l-7.044-3.488a.777.777 0 0 0-1.124.69c0 .294.167.561.43.692l7.865 3.895a1.704 1.704 0 0 0 1.503.004l6.172-3.01a.982.982 0 0 1 1.31.444.97.97 0 0 1-.447 1.302l-6.925 3.377a1.97 1.97 0 0 1-1.736-.005l-9.045-4.48A1.944 1.944 0 0 1 13 29.576V27.85c0-1.445 1.53-2.385 2.832-1.74l7.412 3.67a1.704 1.704 0 0 0 1.512 0l7.944-3.934a.613.613 0 1 0-.552-1.098l-7.274 3.602a1.97 1.97 0 0 1-1.748 0l-9.042-4.477A1.944 1.944 0 0 1 13 22.132v-3.2c0-.765.452-1.46 1.155-1.774l10.09-4.505a7.525 7.525 0 0 1 6.404.127c3.662 1.814 3.585 7.03-.128 8.737l-5.679 2.61a1.97 1.97 0 0 1-1.71-.031l-5.369-2.71a.97.97 0 0 1-.43-1.308.982.982 0 0 1 1.317-.427l4.637 2.341a1.704 1.704 0 0 0 1.479.027l4.933-2.267c2.223-1.022 2.269-4.145.076-5.23Z"
                 clip-rule="evenodd" />
         </svg>
-        <h1 class="h1 h1-bigger text-center max-600 m-0-auto mt-8 mb-64">Panel de administración de concepto</h1>
+        <h1 class="h1 h1-bigger text-center max-600 m-0-auto mt-8 mb-100">Panel de administración de concepto</h1>
 
-        <a href="<?= route('admin.blog') ?>" class="admin-shortcut m-0-auto mb-100">
-            <p>Administrar entradas del blog</p>
-        </a>
+        <div class="shortcuts-container">
+            <a href="<?= route('admin.blog') ?>" class="admin-shortcut m-0-auto">
+                <p>Administrar entradas del blog</p>
+            </a>
 
-        <a href="<?= route('admin.user') ?>" class="admin-shortcut m-0-auto mb-100">
-            <p>Administrar listado de usuarios</p>
-        </a>
+            <a href="<?= route('admin.user') ?>" class="admin-shortcut m-0-auto">
+                <p>Administrar listado de usuarios</p>
+            </a>
+        </div>
 
+    </section>
+
+    <section class="container mt-100 mb-100">
+        <h2 class="h2 mb-24">Estadísticas</h2>
+
+        <h3 class="h3 mb-40">Cursos más vendidos</h3>
+
+        <ul class="container-cards">
+            @foreach ($courses as $course)
+                <li class="course-card">
+                    <a class="anchor-detail" href="<?= route('web.courses.detail', ['id' => $course->course_id]) ?>"
+                        target="_blank">
+                        <div class="card-img">
+                            @if (Str::of($course->cover)->startsWith('https'))
+                                <img src="{{ $course->cover }}" alt="{{ $course->name }}">
+                            @else
+                                <img src="{{ asset('storage/' . $course->cover) }}" alt="{{ $course->name }}">
+                            @endif
+                        </div>
+                        <div class="card-content">
+                            <h3 class="h3 mb-4">{{ $course->name }}</h3>
+                            <p class="card-content-paragraph mb-12">{{ $course->short_description }}</p>
+                            <p class="paragraph fs-6 text-secondary">Cursos vendidos: {{ $course->total_sales }}</p>
+                        </div>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
     </section>
 
 @endsection
