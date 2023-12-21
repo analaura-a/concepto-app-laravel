6241a66766941c6eec49ffe2e36d2f432ac1afc7
Brisa-Marca <brisa.marca@davinci.edu.ar>
2023-12-20 22:29:45 -0300

    delete trending

--- concepto-app/database/migrations/2023_11_19_225448_create_courses_table.php
+++ concepto-app/database/migrations/2023_11_19_225448_create_courses_table.php
@@ -25,7 +25,6 @@ return new class extends Migration
             $table->text('requirements');
             $table->string('tags');
             $table->string('learnings');
-            $table->boolean('trending');
             $table->timestamps();
         });
     }
